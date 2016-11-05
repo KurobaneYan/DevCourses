91f1314179eb83f295b6dce58936790347750190
Yan Kurobane <yan.kurobane@gmail.com>
2016-11-05 13:42:19 +0300

    add PersistenceConfiguration

---
+++ app-persistence/src/main/java/com/netcracker/sd4/persistence/configuration/PersistenceConfiguration.java
@@ -0,0 +1,72 @@
+package com.netcracker.sd4.persistence.configuration;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = {"com.netcracker.sd4.persistence.api.dao.impl"})
+@PropertySource("classpath:persistence.properties")
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.netcracker.sd4.persistence.api.dao")
+public class PersistenceConfiguration {
+
+    private Environment environment;
+
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driver.class"));
+        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public Properties jpaProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.setProperty("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        properties.setProperty("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+
+        return properties;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setDataSource(dataSource());
+        entityManagerFactory.setJpaProperties(jpaProperties());
+        entityManagerFactory.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        entityManagerFactory.setPackagesToScan("com.netcracker.sd4.persistence.domain");
+
+        return entityManagerFactory;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+        return transactionManager;
+    }
+}
