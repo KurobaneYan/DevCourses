7d4608b88f5100bb0fa74041dfb1ac0f4992d5ae
Yan Kurobane <yan.kurobane@gmail.com>
2016-11-17 15:27:44 +0300

    add restTemplate based weather method to WeatherService

--- app-rest/src/main/java/com/netcracker/sd4/rest/controllers/WeatherController.java
+++ app-rest/src/main/java/com/netcracker/sd4/rest/controllers/WeatherController.java
@@ -2,16 +2,21 @@ package com.netcracker.sd4.rest.controllers;
 
 import hello.wsdl.GetCityForecastByZIPResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import weather.WeatherClient;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/weather")
 public class WeatherController {
     private WeatherClient weatherClient;
+    private RestTemplate restTemplate;
 
     @Autowired
     public WeatherController(WeatherClient weatherClient) {
@@ -24,5 +29,26 @@ public class WeatherController {
         GetCityForecastByZIPResponse response = weatherClient.getCityForecastByZIP(zipCode);
         weatherClient.printResponse(response);
         return zipCode;
+    }
+
+    @RequestMapping(value = "/rest", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> getRestWeather() {
+        String city = "Minsk";
+        String appid = "8c4f6b13bec022d5469715859e68bbc3";
+        Map<String, Object> result = (Map<String, Object>) restTemplate
+                .getForObject("http://api.openweathermap.org/data/2.5/weather?q=" +
+                        city + "&appid=" + appid + "&units=metric&lang=ru", Map.class);
+        return result;
+    }
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
