0a4219474e29fb8f3eb01c8ac6399f141b5e83ba
Yan Kurobane <yan.kurobane@gmail.com>
2016-12-11 00:11:28 +0300

    app portlet now can show pagination in table TODO add normal buttons for pagination and maybe add search with filters

---
+++ app-portlet/src/main/java/com/netcracker/sd4/portlet/app/PortletViewController.java
@@ -0,0 +1,67 @@
+package com.netcracker.sd4.portlet.app;
+
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.netcracker.sd4.client.CarClient;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+import org.springframework.web.portlet.bind.annotation.ResourceMapping;
+
+import javax.portlet.ResourceResponse;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("VIEW")
+public class PortletViewController {
+
+	private static final Logger logger = Logger.getLogger(PortletViewController.class);
+
+	private CarClient client;
+
+	@RenderMapping
+	public String render(Model model) {
+		ResponseEntity<String> cars = client.getCarsPagination(1, 2, String.class);
+
+        if (cars == null) {
+        	model.addAttribute("errorMessage", "Could not connect to rest server");
+        	return "app-portlet/error";
+		}
+
+		model.addAttribute("cars", cars.getBody());
+		return "app-portlet/view";
+	}
+
+	@ResourceMapping(value = "getCarPagination")
+	public void getCarPagination(ResourceResponse resourceResponse, @RequestParam("pageSize") int pageSize, @RequestParam("pageNumber") int pageNumber) {
+		logger.info("pageSize: " + String.valueOf(pageSize));
+		logger.info("pageNumber: " + String.valueOf(pageNumber));
+
+		ResponseEntity<String> cars = client.getCarsPagination(pageNumber, pageSize, String.class);
+
+		String result = "Error";
+
+		if (cars != null) {
+			result = cars.getBody();
+		}
+
+		try {
+			JSONArray jsonObject = JSONFactoryUtil.createJSONArray(result);
+			resourceResponse.setContentType("text/json");
+			resourceResponse.getWriter().write(jsonObject.toString());
+		} catch (IOException | JSONException e) {
+			logger.info(e.getMessage());
+		}
+	}
+
+	@Autowired
+	public void setClient(CarClient client) {
+		this.client = client;
+	}
+}
--- app-portlet/src/main/webapp/WEB-INF/jsp/app-portlet/view.jsp
+++ app-portlet/src/main/webapp/WEB-INF/jsp/app-portlet/view.jsp
@@ -2,4 +2,57 @@
 
 <%@ page contentType="text/html" pageEncoding="UTF-8" %>
 
+<portlet:resourceURL id="getCarPagination" var="getCarPagination"/>
+
+<script type="text/javascript" src="${pageContext.request.contextPath}/js/jquery-3.1.1.js"></script>
+<script type="text/javascript">
+    function ajaxCall() {
+        var pageSize = $("#pageSize").val();
+        var pageNumber = $("#pageNumber").val();
+        $.ajax({
+            url: "<%= getCarPagination %>",
+            type: "GET",
+            dataType: "json",
+            data: {
+                <portlet:namespace/>pageSize: pageSize,
+                <portlet:namespace/>pageNumber: pageNumber
+            },
+            success: function (data) {
+                $("#cars-thead").empty().append($("<tr>").append(
+                    $("<th>").text("manufacturer"),
+                    $("<th>").text("model"),
+                    $("<th>").text("year"),
+                    $("<th>").text("price"),
+                    $("<th>").text("style"),
+                    $("<th>").text("left")
+                ));
+                $("#cars-tbody").empty();
+                $.each(data, function (i, car) {
+                    $("<tr>").append(
+                        $("<td>").text(car.manufacturer),
+                        $("<td>").text(car.model),
+                        $("<td>").text(car.productionYear),
+                        $("<td>").text(car.price),
+                        $("<td>").text(car.bodyStyle),
+                        $("<td>").text(car.amountLeft)
+                    ).appendTo("#cars-tbody");
+                });
+            }
+        });
+    };
+</script>
+
 <p><c:out value="${cars}"/></p>
+
+<form>
+    <input id="pageNumber" type="text" name="pageNumber" value="1"/>
+    <input id="pageSize" type="text" name="pageSize" value="3"/>
+    <input type="button" value="getPagination" onclick="ajaxCall()"/>
+</form>
+
+<table id="cars-table" class="table">
+    <thead id="cars-thead">
+    </thead>
+    <tbody id="cars-tbody">
+    </tbody>
+</table>
